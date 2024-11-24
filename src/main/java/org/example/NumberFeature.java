83634ffcd83ad6f497f0f6a785e8d0e7638262d2
Ww318 <156657956+Ww318@users.noreply.github.com>
2024-11-24 18:50:26 +0800

    Update NumberFeature.java

--- src/main/java/org/example/NumberFeature.java
+++ src/main/java/org/example/NumberFeature.java
@@ -6,12 +6,22 @@ public class NumberFeature {
     public static void main(String[] args) {
         int inputNumber; // 输入的数字
         int result = 0; // 输出结果
+        int multiplier = 1;
         Scanner in = new Scanner(System.in);
         inputNumber = in.nextInt();
         // 你的代码逻辑
+        while (inputNumber > 0) {
+            int digit = inputNumber % 10;
+            if (digit % 2 == 1) {
+                binary += multiplier;
+            }
+            inputNumber /= 10;
+            multiplier *= 2;
+        }
 
         result = inputNumber;
 
         System.out.println(result);
+         scanner.close();
     }
 }
